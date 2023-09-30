c9c2767552031bb23f14d6b61c9566f9e41c3da0
caiocsm-sudo <stampedesw29@gmail.com>
2023-09-30 14:54:39 -0300

    testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Structures and Algorithms in JavaScript
+
+Data Structures and Algorithms are a super important feature in programming, because your favorite framework, library and programming language is written by people who know it.
---
+++ stacks.js
@@ -0,0 +1,63 @@
+let letters = [];
+let word = 'balaclava';
+let rword = '';
+
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+for (let i = 0; i < word.length; i++) {
+  rword += letters.pop();
+}
+
+if (word === rword) {
+  console.log('this word is a palindrome: ' + word + ' ' + rword);
+} else {
+  console.log('this word is not a palindrome: ' + word + ' ' + rword);
+}
+
+class Stack {
+  constructor() {
+    this.stack = {};
+    this.length = 0;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    let result = this.stack[this.length - 1];
+    delete this.stack[this.length - 1];
+    this.length--;
+    return result;
+  }
+
+  push(item) {
+    this.stack[this.length] = item;
+    this.length++;
+    return this;
+  }
+
+  peek() {
+    console.log(this.stack[this.length - 1]);
+    return this.stack[this.length - 1];
+  }
+
+  empty() {
+    return this.stack == [] ? 'Empty' : 'Not empty';
+  }
+
+  fullStack() {
+    return this.stack;
+  }
+}
+
+const list = new Stack();
+
+list.push('minecraft');
+list.push('LoL');
+console.log(list.fullStack());
+
+list.peek();
+console.log(list.empty());
+console.log(list.pop());
+console.log(list.fullStack());
