b46215d4074d5e5418e316ccc1f3e830b575f539
caiocsm-sudo <stampedesw29@gmail.com>
2023-10-01 13:12:04 -0300

    priority queue + binary tree

---
+++ files/binaryTree.js
@@ -0,0 +1,20 @@
+// tree = can have any number of child nodes;
+// binary tree = can have only two child nodes;
+
+const Node = class {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+  }
+}
+
--- files/queue.js
+++ files/queue.js
@@ -20,3 +20,49 @@ const Queue = function () {
 };
 
 let q = new Queue();
+
+const PriorityQueue = function () {
+  let collection = [];
+  this.printCollection = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (el) {
+    if (this.isEmpty()) {
+      collection.push(el);
+    } else {
+      let added = false;
+      for (let i = 0; i < collection.length; i++) {
+        if (el[1] < collection[i][1]) {
+          collection.splice(i, 0, el);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        collection.push(el);
+      }
+    }
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+  this.dequeue = function() {
+    let value = collection.shift();
+    return value;
+  }
+  this.front = function() {
+    return collection[0];
+  }
+  this.size = function() {
+    return collection.length;
+  }
+};
+
+const priority = new PriorityQueue();
+
+priority.enqueue(['vanilla js', 2])
+priority.enqueue(['vanilla php', 3]);
+priority.enqueue(['vanilla c', 1]);
+priority.enqueue(['vanilla java', 3]);
+
+priority.printCollection();
